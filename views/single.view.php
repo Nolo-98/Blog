44910a8ed6484dd23594c0bbae28407d71afb5bd
Nolo-98 <twisterfast@gmail.com>
2021-08-11 03:26:32 -0500

    avance

--- views/single.view.php
+++ views/single.view.php
@@ -1,18 +1,17 @@
 <?php require 'header.php'; ?>
 
-         <div class="Contenedor">
-             <div class="post">
-                   <article>
-                     <h2 class="titulo">Titulo del articulo</h2>
-                     <p class="fecha"> 5 de Julio del 2021 </p>
-                     <div class="thumb">
-                            <a href ="=">
-                        <img src="<?php echo RUTA;  ?>/imagenes/1.png" alt="">
-                      <a/>
-                  </div>
-                  <p class="extracto">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla iure perspiciatis, assumenda voluptatibus optio similique consequuntur ullam ipsum totam vel sunt earum ratione nihil alias, nostrum non qui aperiam sequiLorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla iure perspiciatis, assumenda voluptatibus optio similique consequuntur ullam ipsum totam vel sunt earum ratione nihil alias, nostrum non qui aperiam sequiLorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla iure perspiciatis, assumenda voluptatibus optio similique consequuntur ullam ipsum totam vel sunt earum ratione nihil alias, nostrum non qui aperiam sequiLorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla iure perspiciatis, assumenda voluptatibus optio similique consequuntur ullam ipsum totam vel sunt earum ratione nihil alias, nostrum non qui aperiam sequiLorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla iure perspiciatis, assumenda voluptatibus optio similique consequuntur ullam ipsum totam vel sunt earum ratione nihil alias, nostrum non qui aperiam sequi.</p>
-            </article>
-         </div>
+<div class="contenedor">
+    <div class="post">
+        <article>
+            <h2 class="titulo"><?php echo $post['titulo'] ?></h2>
+            <p class="fecha"><?php echo fecha($post['fecha']) ?></p>
+            <div class="thumb">
+                <img src="<?php echo RUTA; ?>/imagenes/<?php echo $post['thumb'] ?>"
+                    alt="<?php echo $post['titulo'] ?>">
+            </div>
+            <p class="extracto"><?php echo nl2br($post['texto']) ?></p>
+        </article>
     </div>
+</div>
 
-    <?php require 'footer.php'; ?>
+<?php require 'footer.php'; ?>
